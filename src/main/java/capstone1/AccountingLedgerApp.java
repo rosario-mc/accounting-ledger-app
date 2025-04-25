4b0de49759e95966a97c652848f8c275c9488ead
Rosario <RMC>
2025-04-25 15:46:38 -0700

    Home Screen Display Completed

--- src/main/java/capstone1/AccountingLedgerApp.java
+++ src/main/java/capstone1/AccountingLedgerApp.java
@@ -1,4 +1,64 @@
 package capstone1;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
-}
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Home Screen
+
+        boolean run = true;
+        while(run){
+            String menu = """
+                    Welcome to The Rosario Reserve!
+                    What can we do for you today?
+                    Please select a transaction from the following options:
+                    D- Add a Deposit
+                    P- Make a Payment (Debit)
+                    L- Ledger
+                    E- Exit
+                    """;
+            System.out.println(menu);
+            String choice = scanner.nextLine();
+            }
+        }
+
+        //Deposit - user input and save to csv
+
+        //Payment - user input save to csv
+
+        //Ledger - display screen
+
+        //Exit
+
+        //Ledger Screen
+
+        //All entries
+
+        //Deposits
+
+        //Payments - only negative entries(payments)
+
+        //Reports - display screen
+
+        //Reports Screen
+
+        //Month to Date
+
+        //Prev Month
+
+        //Year to Date
+
+        //Prev Year
+
+        //Search by Vendor
+
+        //Back to Report Page
+
+        //Home
+
+
+
+    }
+
