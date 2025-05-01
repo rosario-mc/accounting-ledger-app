cdbe6c7959797a3a413e635146193f3181cd0221
Rosario <RMC>
2025-04-30 19:27:34 -0700

    More Utils Added

--- src/main/java/capstone1/Utils.java
+++ src/main/java/capstone1/Utils.java
@@ -1,11 +1,52 @@
 package capstone1;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Utils {
+    public static void printFilteredTransactions(String filename, String header, String filterKeyword) {
+        try {
+            List<String> lines = readAllLines(filename);
+            System.out.println("=======================================================\n" + header + " (Newest First):\n");
+
+            for (int i = lines.size() - 1; i >= 0; i--) {
+                String line = lines.get(i);
+
+                if (filterKeyword == null || line.contains(filterKeyword)) {
+                    System.out.println(line + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("An Error Occurred While Accessing File: " + e.getMessage() + "\n");
+        }
+    }
+    public static void printReportInDateRange(String filename, String header, LocalDate start, LocalDate end) {
+        try {
+            System.out.println("=======================================================\n" + header + ":\n");
+            List<String> lines = readAllLines(filename);
+
+            for (String line : lines) {
+                String[] parts = line.split("\\|");
+                if (parts.length > 0) {
+                    String datePart = parts[0].trim();
+                    LocalDate transactionDate = LocalDate.parse(datePart, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+                    if ((transactionDate.isEqual(start) || transactionDate.isAfter(start)) &&
+                            (transactionDate.isEqual(end) || transactionDate.isBefore(end))) {
+                        System.out.println(line + "\n");
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("An Error Occurred While Accessing File: " + e.getMessage() + "\n");
+        }
+    }
     public static List<String> readAllLines(String filename) throws IOException {
         List<String> lines = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
